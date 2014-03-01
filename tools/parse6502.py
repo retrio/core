b14d5bfd45e87238ebb14c7a738292d8152fe517
bendmorris <ben@bendmorris.com>
2014-03-01 15:08:08 -0500

    Get number of CPU ticks for each operation from opcode table.

--- tools/parse6502.py
+++ tools/parse6502.py
@@ -4,18 +4,33 @@ import re
 alpha = re.compile("[a-zA-Z]")
 
 
+sections = {}
+section_name = re.compile('NAME=\"([A-Z]{3})\"')
+commands = {}
+
 with open("6502table.html") as input_file:
-    p = PyQuery(input_file.read())
-    for tr in p("table.wikitable tr"):
-        tds = tr.findall("td")
-        hx = "0x" + tds[0].text.strip()
-        try: op = tds[1].find("span").find("a")
-        except: continue
-        if op is None: continue
-        op = op.text.strip()
-        md = tds[2].text.strip()
-        if op:
-            md = ''.join(alpha.findall(md))
-            print "case " + hx + ": { code=OpCalls." + op + ";",
-            if (md and md != "Absolute"): print "mode=AddressingModes." + md + ";",
-            print "}"
+    store = None
+    for line in input_file:
+        if '<H3' in line: store = section_name.findall(line)[0]
+        if not store: continue
+        if not store in sections: sections[store] = ''
+        sections[store] += line
+    for op in sections:
+        p = PyQuery(sections[op])
+        table = p("table")[1]
+        rows = table.findall("tr")
+        for tr in rows[1:]:
+            tds = tr.findall("td")
+            i = int(tds[1].find("center").text.strip().lstrip("$"), 16)
+            md = ''.join(alpha.findall(tds[0].find("a").text))
+            ticks = int(tds[3].text.split()[0])
+            commands[i] = (op, md, ticks)
+
+    for k, (op, md, ticks) in sorted(commands.items()):
+        hx = hex(k)[2:].upper()
+        if (len(hx) < 2): hx = "0" + hx
+        hx = "0x" + hx
+        print "case " + hx + ": { code=OpCalls." + op + ";",
+        if (md and md != "Absolute"): print "mode=AddressingModes." + md + ";",
+        if (ticks != 2): print "ticks=%s;" % ticks,
+        print "}"
