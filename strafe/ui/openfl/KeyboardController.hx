23beed03feeaa3a7384e8c7f16683ab0265dd17c
Ben Morris <ben@bendmorris.com>
2015-06-28 21:31:28 -0700

    Remove NES-specific code from KeyboardController.

--- strafe/ui/openfl/KeyboardController.hx
+++ strafe/ui/openfl/KeyboardController.hx
@@ -3,25 +3,12 @@ package strafe.ui.openfl;
 import haxe.ds.Vector;
 import flash.Lib;
 import flash.events.KeyboardEvent;
-// TODO: eliminate
-import strafe.emu.nes.NESControllerButton;
 
 
 class KeyboardController implements IController
 {
 	var _pressed:Vector<Bool> = new Vector(8);
 	var _keyMap:Map<Int, Int> = new Map();
-
-	var defaults:Map<NESControllerButton, Int> = [
-		A => 76,
-		B => 75,
-		Select => 9,
-		Start => 13,
-		Up => 87,
-		Down => 83,
-		Left => 65,
-		Right => 68
-	];
 
 	public function init(e:Dynamic)
 	{
@@ -36,11 +23,7 @@ class KeyboardController implements IController
 	}
 
 	// abstract class
-	public function new()
-	{
-		for (btn in defaults.keys())
-			defineKey(defaults[btn], btn);
-	}
+	public function new() {}
 
 	function onKeyDown(e:Dynamic)
 	{
